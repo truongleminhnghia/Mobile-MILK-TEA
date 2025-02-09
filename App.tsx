543ce049fdf48ef7bfc984f24a9651adef87d573
truongleminhnghia <adronghia@gmail.com>
2025-02-10 01:00:12 +0700

    textinpout

--- App.tsx
+++ App.tsx
@@ -1,13 +1,43 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   // đối tới ts không cần xác định type, ts nó sẽ tự động đoán type tương tự vả bên java
   // nếu muốn ép kiểu thì sau useState<string>("")
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
 
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={{ fontSize: 50, fontWeight: "600" }}>Name: {name}</Text>
+        <TextInput
+          // multiline giống như textarea
+          multiline
+          onChangeText={(value) => setName(value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+          }}
+        />
+      </View>
+      <View>
+        <Text style={{ fontSize: 50, fontWeight: "600" }}>Tuổi: {age}</Text>
+        <TextInput
+          // multiline giống như textarea
+          multiline
+          onChangeText={(value) => setAge(value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+          }}
+        />
+      </View>
       <Text style={{ fontSize: 60, fontWeight: "600" }}>Count = {count}</Text>
       <View>
         <Button title="InCrease" onPress={() => setCount(count + 1)} />
